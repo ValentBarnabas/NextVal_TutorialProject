ad52ce10c6500b6d0ccf9cab0ad7f6c9506d94a0
ValentBarnabas <78793539+ValentBarnabas@users.noreply.github.com>
2022-06-20 11:33:51 +0200

    Delete imdb-app/build directory

--- imdb-app/build/tmp/kapt3/stubs/main/hu/nextval/imdb/app/academyofactors/JPAAcademyOfActorsRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.nextval.imdb.app.academyofactors;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0000\bg\u0018\u00002\u00020\u00012\u000e\u0012\u0004\u0012\u00020\u0003\u0012\u0004\u0012\u00020\u00040\u0002\u00a8\u0006\u0005"}, d2 = {"Lhu/nextval/imdb/app/academyofactors/JPAAcademyOfActorsRepository;", "Lhu/nextval/imdb/app/academyofactors/AcademyOfActorsRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Lhu/nextval/imdb/app/academyofactors/AcademyOfActorsEntity;", "", "imdb-app"})
-@org.springframework.stereotype.Repository
-@org.springframework.context.annotation.Primary
-public abstract interface JPAAcademyOfActorsRepository extends hu.nextval.imdb.app.academyofactors.AcademyOfActorsRepository, org.springframework.data.jpa.repository.JpaRepository<hu.nextval.imdb.app.academyofactors.AcademyOfActorsEntity, java.lang.Long> {
-}
--- imdb-app/build/tmp/kapt3/stubs/main/hu/nextval/imdb/app/movie/JPAMovieRepository.java
+++
@@ -1,9 +0,0 @@
-package hu.nextval.imdb.app.movie;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0000\bg\u0018\u00002\u00020\u00012\u000e\u0012\u0004\u0012\u00020\u0003\u0012\u0004\u0012\u00020\u00040\u0002\u00a8\u0006\u0005"}, d2 = {"Lhu/nextval/imdb/app/movie/JPAMovieRepository;", "Lhu/nextval/imdb/app/movie/MovieRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Lhu/nextval/imdb/app/movie/MovieEntity;", "", "imdb-app"})
-@org.springframework.stereotype.Repository
-@org.springframework.context.annotation.Primary
-public abstract interface JPAMovieRepository extends hu.nextval.imdb.app.movie.MovieRepository, org.springframework.data.jpa.repository.JpaRepository<hu.nextval.imdb.app.movie.MovieEntity, java.lang.Long> {
-}
